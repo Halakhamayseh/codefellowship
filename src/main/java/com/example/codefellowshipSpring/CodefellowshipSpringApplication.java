4f4388263ba1d9f4ac3cc17a6f72c6566c73a908
Halakhamayseh <khamaysehhala95@yahoo.com>
2021-08-15 20:42:42 +0300

    first push

---
+++ src/main/java/com/example/codefellowshipSpring/CodefellowshipSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.codefellowshipSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodefellowshipSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodefellowshipSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codefellowshipSpring/CodefellowshipSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codefellowshipSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodefellowshipSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
