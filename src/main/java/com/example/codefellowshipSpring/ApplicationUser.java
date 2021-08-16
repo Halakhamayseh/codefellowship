e2c195a25d096e74f432e33320c7e99a3ea05383
Halakhamayseh <khamaysehhala95@yahoo.com>
2021-08-17 00:16:12 +0300

    done

--- src/main/java/com/example/codefellowshipSpring/ApplicationUser.java
+++ src/main/java/com/example/codefellowshipSpring/ApplicationUser.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.Collection;
+import java.util.List;
 //import java.util.Date;// we test and it is not working for dateofbirth
 
 @Entity
@@ -17,9 +18,11 @@ public class ApplicationUser implements UserDetails {
     String password;
     String firstname;
     String lastname;
-    Integer dateofbirth;
+    String dateofbirth;
     String bio;
-    public ApplicationUser(String username, String password, String firstname, String lastname, Integer dateofbirth,String bio) {
+@OneToMany(mappedBy = "applicationUser")
+    List<Post> allposts;
+    public ApplicationUser(String username, String password, String firstname, String lastname, String dateofbirth,String bio) {
         this.username = username;
         this.password = password;
         this.firstname = firstname;
@@ -33,10 +36,36 @@ public class ApplicationUser implements UserDetails {
         //default one
     }
 
+    public List<Post> getAllposts() {
+        return allposts;
+    }
+
+    public void setAllposts(List<Post> allposts) {
+        this.allposts = allposts;
+    }
+
     public void setUsername(String username){
         this.username = username;
     }
 
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getDateofbirth() {
+        return dateofbirth;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+    public Integer getId() {
+        return id;
+    }
     public void setPassword(String password){
         this.password = password;
     }
@@ -49,16 +78,16 @@ public class ApplicationUser implements UserDetails {
         this.lastname = lastname;
     }
 
-    public void setDateOfBirth(Integer dateofbirth) {
+    public void setDateOfBirth(String dateofbirth) {
+
         this.dateofbirth = dateofbirth;
     }
 
     public void setBio(String bio) {
         this.bio = bio;
     }
-    public Integer getId() {
-        return id;
-    }
+
+
 
 
 
@@ -98,113 +127,3 @@ public class ApplicationUser implements UserDetails {
     }
 
 }
-//
-//
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
-//
-//import javax.persistence.*;
-//import javax.xml.crypto.Data;
-//import java.util.Collection;
-//
-//@Entity
-//public class ApplicationUser implements UserDetails {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    public Integer getId() {
-//        return id;
-//    }
-//      Integer id;
-//    @Column(unique = true)
-//    private String username;
-//
-//    private String password;
-//    private String firstname;
-//    private String lastname;
-//    private String dateofbirth;
-//    private String   bio;
-//
-//    public ApplicationUser(String username, String password, String firstname, String lastname, String dateofbirth, String bio) {
-//        this.username = username;
-//        this.password = password;
-//        this.firstname = firstname;
-//        this.lastname = lastname;
-//        this.dateofbirth = dateofbirth;
-//        this.bio = bio;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//
-//
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public String getFirstname() {
-//        return firstname;
-//    }
-//
-//    public void setFirstname(String firstname) {
-//        this.firstname = firstname;
-//    }
-//
-//    public String getLastname() {
-//        return lastname;
-//    }
-//
-//    public void setLastname(String lastname) {
-//        this.lastname = lastname;
-//    }
-//
-//    public String getBio() {
-//        return bio;
-//    }
-//
-//    public void setBio(String bio) {
-//        this.bio = bio;
-//    }
-//
-//
-//
-//    public ApplicationUser() {
-//        //Default Cons
-//    }
-//
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        return null;
-//    }
-//
-//    @Override
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    @Override
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonLocked() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isCredentialsNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isEnabled() {
-//        return true;
-//    }
-//}
